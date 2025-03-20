c5b68a557a88153d8c925b4286afacf70e37b643
Syaikhasril <153400022+Syaasr@users.noreply.github.com>
2025-03-20 13:00:30 +0700

    Update Arithmetic.java

--- Tugas 2/Arithmetic.java
+++ Tugas 2/Arithmetic.java
@@ -2,7 +2,6 @@ public class Arithmetic {
     public static void main(String[] args) {
         int a = 25;
         int b = 12;
-        int c;
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
