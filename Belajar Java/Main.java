7a8f0e60d0d7fbefd7d0dc7921b23d37086760c0
Syaikhasril <153400022+Syaasr@users.noreply.github.com>
2025-04-10 09:58:00 +0700

    Update Main.java

--- Belajar Java/Main.java
+++ Belajar Java/Main.java
@@ -5,8 +5,7 @@ public class Main {
         double IPK = 3.91;
         boolean isSingle = true;
         String name = "Syaikhasril Maulana Firdaus";
-        int uang;
-
+        
         System.out.println("Nama : " + name + "\nUmur : " + age + "\nIPK : " + IPK);
 
         int a = 10, b = 20;
